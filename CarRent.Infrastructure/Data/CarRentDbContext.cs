7bbd6c28f42a692d0c4f81489de7a0ac60578863
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-03-17 14:29:31 +0200

    Configure DbContext

---
+++ CarRent.Infrastructure/Data/CarRentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CarRent.Infrastructure.Data.Models.Cars;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRent.Infrastructure.Data
+{
+    public class CarRentDbContext : IdentityDbContext<IdentityUser>
+    {
+        public CarRentDbContext(DbContextOptions<CarRentDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
--- CarRent/Startup/DependenciesConfig.cs
+++ CarRent/Startup/DependenciesConfig.cs
@@ -1,9 +1,20 @@
-﻿namespace CarRent.Startup
+﻿using CarRent.Infrastructure.Data;
+using CarRent.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRent.Startup
 {
     public static class DependenciesConfig
     {
         public static void AddDependencies(this WebApplicationBuilder builder)
         {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<CarRentDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+            builder.Services.AddScoped<IRepository, Repository>();
+
             builder.Services.AddControllers();
             builder.Services.AddOpenApiServices();
         }
