3d7a8dd89b0842c96e06668c35b6a7868abb91f6
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-05-31 16:35:38 +0300

    Set up main App component and entry point

---
+++ CarRent/app/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>Car Management System</h1>
+            <h3>Welcome to CarRent</h3>
+            <p>The best place to rent a car!</p>
+        </div>
+    );
+}
+
+export default App;
