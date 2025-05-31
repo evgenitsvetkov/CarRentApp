edb3ce692f6443c9e696b6ce2f0bb1152891f5de
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-05-31 16:32:47 +0300

    Add routing

---
+++ CarRent/app/src/router.js
@@ -0,0 +1,21 @@
+﻿import { createBrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import Layout from "./components/Layout";
+import Cars from "./components/Cars";
+import AddCar from "./components/AddCar";
+import CarDetails from "./components/CarDetails";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />, 
+        children: [
+            { path: "/", element: <App /> },
+            { path: "/Cars", element: <Cars /> },
+            { path: "/Cars/Details/:id", element: <CarDetails /> },
+            { path: "/Cars/Add", element: <AddCar /> },
+        ],
+    },
+]);
+
+export default router;
