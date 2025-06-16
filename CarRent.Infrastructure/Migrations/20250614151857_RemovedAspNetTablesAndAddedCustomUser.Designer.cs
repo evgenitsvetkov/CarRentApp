82049958fa757414d4219ad983003d7082b0f1d2
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-06-16 23:37:15 +0300

    Replace ASP.NET Identity with custom User entity

--- CarRent.Infrastructure/Migrations/20250614151857_RemovedAspNetTablesAndAddedCustomUser.Designer.cs
+++ CarRent.Infrastructure/Migrations/20250614151857_RemovedAspNetTablesAndAddedCustomUser.Designer.cs
@@ -4,6 +4,7 @@ using CarRent.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRent.Infrastructure.Migrations
 {
     [DbContext(typeof(CarRentDbContext))]
-    partial class CarRentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250614151857_RemovedAspNetTablesAndAddedCustomUser")]
+    partial class RemovedAspNetTablesAndAddedCustomUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
