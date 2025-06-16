c8dc8c13ebe8a9ffa26d0951f378a5abf9450913
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-06-16 23:44:06 +0300

    Add UserDto model

---
+++ CarRent.Core/Models/User/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRent.Core.Models.User
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage = "The {0} field is required")]
+        [StringLength(50,
+            MinimumLength = 5,
+            ErrorMessage = "The field {0} must be between {2} and {1} characters long")]
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
