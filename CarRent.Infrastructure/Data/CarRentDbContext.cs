e7b91b1ee1113137dc09d8481621fdd9d575056c
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-06-16 23:34:59 +0300

    Add custom User entity

--- CarRent.Infrastructure/Data/CarRentDbContext.cs
+++ CarRent.Infrastructure/Data/CarRentDbContext.cs
@@ -1,19 +1,18 @@
 ﻿using CarRent.Infrastructure.Data.Models.Cars;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using CarRent.Infrastructure.Data.Models.CustomUser;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRent.Infrastructure.Data
 {
-    public class CarRentDbContext : IdentityDbContext<IdentityUser>
+    public class CarRentDbContext : DbContext
     {
         public CarRentDbContext(DbContextOptions<CarRentDbContext> options) : base(options) 
-        {
-
-        }
+        { }
 
         public DbSet<Car> Cars { get; set; } = null!;
 
         public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<User> Users { get; set; } = null!;
     }
 }
---
+++ CarRent.Infrastructure/Data/Models/CustomUser/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRent.Infrastructure.Data.Models.CustomUser
+{
+    public class User
+    {
+        [Key]
+        [Comment("User's identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Comment("User's username")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("User's hashed password")]
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
