de6b5c9471679f83ac3a4c69c05387b5e8b0d6d0
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-06-17 16:40:14 +0300

    Add DTOs for refresh token request and response

---
+++ CarRent.Core/Models/User/RefreshTokenRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRent.Core.Models.User
+{
+    public class RefreshTokenRequestDto
+    {
+        public Guid UserId { get; set; }
+
+        public required string RefreshToken { get; set; }
+
+    }
+}
---
+++ CarRent.Core/Models/User/TokenResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRent.Core.Models.User
+{
+    public class TokenResponseDto
+    {
+        public required string AccessToken { get; set; }
+        
+        public required string RefreshToken { get; set; }
+    }
+}
