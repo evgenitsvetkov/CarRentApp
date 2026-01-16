c334dc57038067016f957cddba6471c647bd19c8
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2026-01-16 17:44:29 +0200

    Add shared axios instances for public and private requests

---
+++ CarRent/app/src/api/axios.js
@@ -0,0 +1,17 @@
+﻿import axios from 'axios';
+
+const API_BASE_URL = "https://localhost:7091";
+
+export const apiClient = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
