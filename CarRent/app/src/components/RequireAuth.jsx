283e16ca251c7bec980c7f74d762be071f33a0f7
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2026-03-02 19:26:57 +0200

    Simplify RequireAuth

--- CarRent/app/src/components/RequireAuth.jsx
+++ CarRent/app/src/components/RequireAuth.jsx
@@ -1,34 +1,19 @@
-import { useLocation, Navigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import { Outlet, useLocation, Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
-const RequireAuth = ({ children, requiredRole }) => {
+const RequireAuth = ({ requiredRole }) => {
     const { auth } = useAuth();
     const location = useLocation();
 
-    const token = auth?.accessToken;
-    let hasAccess = false;
-
-    if (token) {
-        try {
-            const decoded = jwtDecode(token);
-            const role = decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
-
-            hasAccess = role === requiredRole
-                ? true
-                : false;
-        } catch (err) {
-            console.error('Invalid token:', err);
-        }
+    if (!auth?.accessToken) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
-    return (
-        hasAccess
-            ? children
-            : auth?.user
-                ? <Navigate to="/unauthorized" state={{ fromProtected: true, from: location }} replace />
-                : <Navigate to="/login" state={{ from: location }} replace />
-    );
+    if (auth.role !== requiredRole) {
+        return <Navigate to="/unauthorized" state={{ fromProtected: true, from: location }} replace />;
+    }
+
+    return <Outlet />;
 };
 
 export default RequireAuth;
